a579de1e352ee118cb402b61ad5e5c556b687e29
Monica Gerard <mlgvla@users.noreply.github.com>
2021-12-23 03:42:23 +0000

    manipulating lists

---
+++ listmethods/listmeth02.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+proto = ["ssh", "http", "https"]
+protoa = ["ssh", "http", "https"]
+print(proto)
+proto.append("dns") # this line will add "dns" to the end of our list
+protoa.append("dns") # this line will add "dns" to the end of our list
+print(proto)
+proto2 = [ 22, 80, 443, 53 ] # a list of common ports
+proto.extend(proto2) # pass proto2 as an argument to the extend method
+print(proto)
+protoa.append(proto2) # pass proto2 as an argument to the append method
+print(protoa)
+my_pets = ["dog", "cat"]
+print(my_pets)
+my_pets.append("bird")
+print(my_pets)
+zoo_animals = ["lion", "tiger", "bear"]
+my_pets.extend(zoo_animals)
+print(my_pets)
